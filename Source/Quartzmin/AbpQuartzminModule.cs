30b365bcb9b95a24ab392d6a7982ce301f4ee8b6
xyfy <benguahao@gmail.com>
2021-12-06 13:55:46 +0800

    兼容abp模块处理

---
+++ Source/Quartzmin/AbpQuartzminModule.cs
@@ -0,0 +1,39 @@
+﻿using Abp.Configuration.Startup;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartzmin
+{
+    [DependsOn(typeof(Abp.AspNetCore.AbpAspNetCoreModule))]
+    [DependsOn(typeof(Abp.Quartz.AbpQuartzModule))]
+    public class AbpQuartzminModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            base.PreInitialize();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(AbpQuartzminModule).GetAssembly());
+        }
+    }
+
+}
+
+namespace Quartzmin.Controllers
+{
+    [IgnoreAntiforgeryToken]
+    public abstract partial class PageControllerBase : Abp.Dependency.ITransientDependency
+    {
+
+    }
+
+}
+
